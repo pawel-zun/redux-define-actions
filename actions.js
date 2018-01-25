c6ee8388326b956decdfe80f968ec009152b04fb
Pawel Zun <pzun90@gmail.com>
2018-01-25 22:51:56 +0100

    create action types example

---
+++ actions.js
@@ -0,0 +1,50 @@
+import uuid from uuid;
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const DELETE_COMMENT = 'DELETE_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+  return {
+    type: ADD_COMMENT,
+    text: text,
+    id: uuid.v4()
+  }
+}
+
+function editComment(id, text) {
+  return {
+    type: EDIT_COMMENT,
+    id,
+    text
+  }
+}
+
+function deleteComment(id) {
+  return {
+    type: DELETE_COMMENT,
+    id
+  }
+}
+
+function thumbUpComment(id) {
+  return {
+    type: THUMB_UP_COMMENT,
+    id
+  }
+}
+
+function thumbDownComment(id) {
+  return {
+    type: THUMB_DOWN_COMMENT,
+    id
+  }
+}
+
+const boundAddComment = text => dispatch(addComment(text));
+const boundEditComment = (id, text) => dispatch(editComment(id, text));
+const boundDeleteComment = (id) => dispatch(deleteComment(id));
+const boundThumbUpComment = (id) => dispatch(thumbUpComment(id));
+const boundThumbDownComment = (id) => dispatch(thumbDownComment(id));

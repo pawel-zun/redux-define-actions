306d08f8200350db3fda702806524b7f301ab27f
Pawel Zun <pzun90@gmail.com>
2018-01-28 19:10:59 +0100

    extract sub-reducers

--- reducer.js
+++ reducer.js
@@ -1,40 +1,8 @@
-import { ADD_COMMENT, DELETE_COMMENT, EDIT_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT } from './actions';
+import { combineReducers } from 'redux';
+import comments from './comments';
+import users from './users';
 
-const initialState = {
-  comments: [],
-  users: []
-};
-
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case ADD_COMMENT:
-      return Object.assign({}, state, {
-        comments: [
-          {
-            id: action.id,
-            text: action.text,
-            votes: 0
-          },
-          ...state
-        ]
-      });
-    case DELETE_COMMENT:
-      return Object.assign({}, state, {
-        comments: state.comments.filter(comment => comment.id !== action.id)
-      });
-    case EDIT_COMMENT:
-      return Object.assign({}, state, {
-        comments: state.comments.map(comment => comment.id === action.id ? Object.assign(comment, {text: action.text}) : comment)
-      });
-    case THUMB_UP_COMMENT:
-      return Object.assign({}, state, {
-        comments: state.comments.map(comment => comment.id === action.id ? Object.assign(comment, {votes: comment.votes + 1}) : comment)
-      });
-    case THUMB_DOWN_COMMENT:
-      return Object.assign({}, state, {
-        comments: state.comments.map(comment => comment.id === action.id ? Object.assign(comment, { votes: comment.votes - 1 }) : comment)
-      });
-    default:
-      return state;
-  }  
-}
+const app = combineReducers({
+  comments,
+  users
+});
---
+++ users.js
@@ -0,0 +1,5 @@
+function users() {
+  
+}
+
+export default users;
